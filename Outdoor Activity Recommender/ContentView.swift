8f318f63ce9035586906a417f998131e77759064
tg37 <tg37@hw.ac.uk>
2020-12-29 00:45:19 +0000

    Initial Commit

---
+++ Outdoor Activity Recommender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Outdoor Activity Recommender
+//
+//  Created by THOMAS GRAY on 29/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Outdoor Activity Recommender/Outdoor_Activity_RecommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outdoor_Activity_RecommenderApp.swift
+//  Outdoor Activity Recommender
+//
+//  Created by THOMAS GRAY on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Outdoor_Activity_RecommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
